dc5dd1e8ae23849777afdaf1f99497031366874f
arcgittest <arcgittest@gmail.com>
2019-07-25 12:55:38 -0400

    added more chrome options

--- src/test/java/com/archs/testCases/TestBase.java
+++ src/test/java/com/archs/testCases/TestBase.java
@@ -50,6 +50,8 @@ public class TestBase {
 		options.addArguments("disable-infobars");
 		options.addArguments("--disable-notifications");
 		options.addArguments("--disable-features=VizDisplayCompositor");
+		options.addArguments("--headless");
+	    options.addArguments("--window-size=1920,1080");
 		driver=new ChromeDriver(options);
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
